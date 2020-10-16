7bafb42747367a7e3bf09c8841b94d3ea8efd8de
trepear <tbuhrsmith@gmail.com>
2020-10-16 12:59:45 -0500

    finished contact form and getting ready for heroku deployment

---
+++ server.js
@@ -0,0 +1,34 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+const logger = require('morgan');
+const path = require('path');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"))
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/home.html"));
+});
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
